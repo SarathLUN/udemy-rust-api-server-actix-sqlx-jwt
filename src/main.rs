2dccee20e52a7b383a98209ea48231aa135fa4b1
SarathLUN <lun.sarath@gmail.com>
2025-04-18 00:54:27 +0700

    finish lesson 6 run actix-web helloworld

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpResponse, HttpServer, Responder, get, post, web};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+// curl http://localhost:8080/
+// -> Hello world!
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+// curl http://localhost:8080/hey
+// -> Hey there!
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
+// curl -d "Hello ECHO" -X POST http://localhost:8080/echo
+// -> Hello ECHO
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
